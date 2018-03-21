c157e7c2438981124692881ccb3934ffa7369140
MegaKruk <slayermazi@gmail.com>
2018-03-21 21:02:43 +0100

    working!

--- etap2n/main.cpp
+++ etap2n/main.cpp
@@ -9,27 +9,29 @@
 constexpr int noOfPhils = 5;
 std::thread philosphers[noOfPhils];
 std::mutex fork[noOfPhils]; 	//forks as mutexes
-std::mutex cout_mutex; 			// mutex for keeping cout safe
+std::mutex myMutex; 			// mutex for keeping cout safe
 int ate[5] = { 0 }; 				// Counting which philosopher ate how many times
 bool stop = false;
 
 void safePrint(std::string str)
 {
-	cout_mutex.lock(); 			// Locking cout, so that no other operations will intrupt
+	myMutex.lock(); 			// Locking cout, so that no other operations will intrupt
 	std::cout << std::endl << str << std::endl; // old
-	cout_mutex.unlock();
+	myMutex.unlock();
 }
 
 void menuInit()
 {
 	for (int i = 0; i < noOfPhils; i++)
 	{
+		myMutex.lock();
 		move(i, 0);
 		printw("Philosopher[%d] idling", i);
 		refresh();
 		move(i + 6, 0);
 		printw("Fork[%d] is free", i);
 		refresh();
+		myMutex.unlock();
 		//safePrint("Philosopher " + std::to_string(id) + " is thinking " + std::to_string(i/randThink) + " %");
 	}
 }
@@ -42,11 +44,13 @@ void think(int id)
 	for (int i = 1; i <= randThink; i++)
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		move(id, 0);
 		progressT = (100 * i) / randThink;
+		myMutex.lock();
+		move(id, 0);
 		clrtoeol();
 		printw("Philosopher[%d] is thinking:\t%.0f\tpercent done", id, progressT);
 		refresh();
+		myMutex.unlock();
 		//safePrint("Philosopher " + std::to_string(id) + " is thinking " + std::to_string(i/randThink) + " %");
 	}
 	//safePrint("Philosopher " + std::to_string(id) + " has stopped thinking");
@@ -58,17 +62,21 @@ bool eat(int id, int left, int right)
 	// Philosopher[id] trying to pick up left fork, keeps going until success
 	while (1) if (fork[left].try_lock())
 	{
+		myMutex.lock();
 		//safePrint("Philosopher " + std::to_string(id) + " got the fork " + std::to_string(left));
 		move(left + 6, 0);
 		clrtoeol();
 		printw("Fork[%d] is taken", left);
 		refresh();
+		myMutex.unlock();
 		if (fork[right].try_lock())
 		{
+			myMutex.lock();
 			move(right + 6, 0);
 			clrtoeol();
 			printw("Fork[%d] is taken", right);
 			refresh();
+			myMutex.unlock();
 			// Philosopher[id] trying to pick up right fork
 			//safePrint("Philosopher " + std::to_string(id) + " got the fork " + std::to_string(right)); 
 			int randEat = (std::rand() % 30) + 20;
@@ -77,11 +85,13 @@ bool eat(int id, int left, int right)
 			for (int i = 1; i <= randEat; i++)
 			{
 				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				move(id, 0);
 				progressE = (100 * i) / randEat;
+				myMutex.lock();
+				move(id, 0);
 				clrtoeol();
 				printw("Philosopher[%d] is eating:\t%.0f\tpercent done", id, progressE);
 				refresh();
+				myMutex.unlock();
 				//safePrint("Philosopher " + std::to_string(id) + " is thinking " + std::to_string(i/randThink) + " %");
 			}
 			//safePrint("Philosopher " + std::to_string(id) + " has stopped eating");
@@ -91,27 +101,34 @@ bool eat(int id, int left, int right)
 		{
 			// Drop first fork if can't pick up other to prevent deadlock and starvation
 			fork[left].unlock();
+			myMutex.lock();
 			move(left + 6, 0);
 			clrtoeol();
 			printw("Fork[%d] is free", left);
 			refresh();
+			myMutex.unlock();
 		}
+		//myMutex.unlock();
 	}
 }
 
 void putDownForks(int left, int right)
 {
 	fork[left].unlock();
+	myMutex.lock();
 	move(left + 6, 0);
 	clrtoeol();
 	printw("Fork[%d] is free", left);
 	refresh();
+	myMutex.unlock();
 
 	fork[right].unlock();
+	myMutex.lock();
 	move(right + 6, 0);
 	clrtoeol();
 	printw("Fork[%d] is free", right);
 	refresh();
+	myMutex.unlock();
 }
 
 void dinner_started(int philID)
@@ -139,23 +156,17 @@ int main()
 {
 	srand(time(NULL));
 	initscr();
+	cbreak();
 	menuInit();
-	raw();
-	/*
-	initscr();
-	printw("Hello World !!!");
-	refresh();
-	getch();
-	endwin();
-	*/
+	//raw();
 	// creating threads
 	for (int i = 0; i < noOfPhils; ++i)
 		philosphers[i] = std::thread(dinner_started, i);
-
+	
 	std::cin.get(); 	// pause main thread here, philosophers still going
 	stop = true;		// this breaks philosophers' main loop
 
-						  // joining threads
+	// joining threads
 	for (int i = 0; i < noOfPhils; ++i)
 		philosphers[i].join();
 
